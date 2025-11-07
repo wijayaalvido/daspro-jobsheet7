a18967af478e154c8eaa4dda5d1b6554e58374fd
Alvido Wijaya <wijayaalvido@gmail.com>
2025-11-07 09:29:46 +0700

    Modifikasi Loop For

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -3,19 +3,31 @@ public class SiakadFor05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            
+            if (nilai >= 60){
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
+    System.out.println("\n\t\t\t=== HASIL AKHIR ===");
     System.out.println("Nilai tertinggi : " + tertinggi);
     System.out.println("Nilai terendah  : " + terendah);
+    System.out.println("Jumlah mahasiswa yang lulus : " + lulus);
+    System.out.println("Jumlah mahasiswa yang tidak lulus : " + tidakLulus);
+    sc.close();
             }
         }
